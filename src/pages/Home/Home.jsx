b7dc1a50658012006f5fdb86a965e4ff83059387
Rafael <rafaelbrandaobastos@gmail.com>
2023-12-21 12:38:02 -0300

    core: first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React, { } from "react";
+import { } from "./style";
+import "./style.js";
+  
+  const Home = () => {
+    return (
+      <>
+      </>
+    );
+  };
+  
+export default Home;
+  
